9605d023263e61e98380f5101639ec82b5356ffe
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2025-05-03 00:14:11 +0300

    added parent nodes

--- src/test_leafnode.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from htmlnode import LeafNode
-
-class TestLeafNode(unittest.TestCase):
-    def test_leaf_to_html_p(self):
-        node = LeafNode("p", "Hello, world!")
-        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
