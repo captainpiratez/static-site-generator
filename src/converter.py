ab77cad51bfa532e2db9a14f5340009cdd3e4a8f
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2025-06-01 02:47:28 +0300

    first implementation of split_nodes_delimiter fn

--- src/converter.py
+++ src/converter.py
@@ -4,14 +4,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
         if node.text_type == TextType.TEXT:
-            if node.text_type == TextType.TEXT:
-                parts = node.text.split(delimiter)
-                for i, part in enumerate(parts):
-                    if i % 2 == 0:
-                        node_type = TextType.TEXT
-                    else:
-                        node_type = text_type
-                    new_nodes.append(TextNode(part, node_type))
-            else:
-                new_nodes.append(node)
+            parts = node.text.split(delimiter)
+            if len(parts) % 2 == 0:
+                raise Exception("Unmatched delimiter!")
+            for i, part in enumerate(parts):
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(part, TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(part, text_type))
+        else:
+            new_nodes.append(node)
     return new_nodes
