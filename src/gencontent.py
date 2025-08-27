91dc4c56c87c3e3006926e23bcad96462d2493f6
captainpiratez <102522988+captainpiratez@users.noreply.github.com>
2025-08-27 03:45:41 +0300

    gen content recursive - my version

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+
 from markdown_blocks import markdown_to_html_node
 
 
@@ -25,6 +27,19 @@ def generate_page(from_path, template_path, dest_path):
     to_file = open(dest_path, "w")
     to_file.write(template)
 
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    ls = os.listdir(dir_path_content)
+    for file in ls:
+        file_path = os.path.join(dir_path_content, file)
+        path = Path(file_path)
+        if os.path.isfile(file_path):
+            if path.suffix == ".md":
+                dest_file_path = Path(os.path.join(dest_dir_path, file)).with_suffix('.html')
+                generate_page(file_path, template_path, dest_file_path)
+        else:
+            dest_file_path = os.path.join(dest_dir_path, file)
+            generate_pages_recursive(file_path, template_path, dest_file_path)
 
 def extract_title(md):
     lines = md.split("\n")
